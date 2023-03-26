2c508447be91d18c892280ad972228de14fb781c
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-03-27 00:08:16 +0100

    data types

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,7 @@
+let country = "England";
+let continent = "Europe";
+let population ="55.98 million (2018)";
+
+console.log(country);
+console.log(continent);
+console.log(population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,25 +1,60 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
-console.log("Petar");
-console.log(23);
+// console.log("Petar");
+// console.log(23);
 
-let firstName = "Matilda";
+// let firstName = "Matilda";
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
-let petar_martilda = "PM";
-let $function = 27;
+// let petar_martilda = "PM";
+// let $function = 27;
 
-let person = "petar";
-let PI = 3.1415;
+// let person = "petar";
+// let PI = 3.1415;
 
-let myFirstJob = "Programer";
-let myCurrentJob = "Teacher";
+// let myFirstJob = "Programer";
+// let myCurrentJob = "Teacher";
 
-let job1 = "programer";
-let job2 = "teacher";
+// let job1 = "programer";
+// let job2 = "teacher";
 
-console.log(myFirstJob);
+// console.log(myFirstJob);
+
+// true;
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Petar");
+
+// javascriptIsFun = "YES!";
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1987;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+
+let age = 35;
+age = 36
+
+const birthYear = 1987;
+// birthYear = 1988;
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Nikolov";
+console.log(lastName);
