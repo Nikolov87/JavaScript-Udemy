8fb429fdb3365ccf35c1c821120052c126d4c828
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-03-26 21:58:57 +0100

    values and variables

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,25 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Petar");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let petar_martilda = "PM";
+let $function = 27;
+
+let person = "petar";
+let PI = 3.1415;
+
+let myFirstJob = "Programer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
