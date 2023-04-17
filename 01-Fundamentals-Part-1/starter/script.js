8fe6a0d580b3752281d09ff5277aec390f2cdf94
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-04-17 09:25:18 +0100

    21. Truthy and Falsy Values

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -206,22 +206,48 @@ if (bmiMark > bmiJohn) {
 
 
 /* 20. Type Conversion and Coercion  */
-const inputYear = '1987 ';
-console.log(Number(inputYear)); //converting strings to numbers
-console.log(Number(inputYear) + 18);
 
-console.log(Number('Petar'));
-console.log(typeof NaN);
+// const inputYear = '1987 ';
+// console.log(Number(inputYear)); //converting strings to numbers
+// console.log(Number(inputYear) + 18);
 
-// converting strings to numbers
-console.log(String(35, 35));
+// console.log(Number('Petar'));
+// console.log(typeof NaN);
 
-// type coercion
-console.log('I am ' + 35 + ' years ols');
-// console.log('I am ' + String(35) + ' years ols');
-console.log('35' - '10' - 3); //strings to numbers
-console.log('35' / '2');
+// // converting strings to numbers
+// console.log(String(35, 35));
 
-let n = '1' + 1;   // '1' + 1 = 11
-n = n - 1;   // 11 - 1 = 10
-console.log(n);   // output 11
+// // type coercion
+// console.log('I am ' + 35 + ' years ols');
+// // console.log('I am ' + String(35) + ' years ols');
+// console.log('35' - '10' - 3); //strings to numbers
+// console.log('35' / '2');
+
+// let n = '1' + 1;   // '1' + 1 = 11
+// n = n - 1;   // 11 - 1 = 10
+// console.log(n);   // output 11
+
+
+
+/* 21. Truthy and Falsy Values */
+// 5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Petar'));
+console.log(Boolean({})); 
+console.log(Boolean('')); 
+
+const money = 0;
+if(money) {
+    console.log("Don't spend it all :)");
+} else {
+    console.log('You should get a job!');
+}
+
+let height = 0;
+if(height) {
+    console.log('YAY! Height is defined');
+} else {
+    console.log('Height is UNDEFINED'); 
+}
