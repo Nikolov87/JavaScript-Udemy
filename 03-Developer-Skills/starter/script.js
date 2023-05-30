8a6535e493cd2e0c1396bb89decbd9f485d15e97
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-05-30 13:01:41 +0100

    60. Debugging(Fixing Errors)

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -15,7 +15,7 @@
 // calculate the temperature amplitude. Keep in mind that sometimes there
 // might be a sensor error."
 
-const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+// const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 
 //1. Udnderstanding the problem
 // - What is the temp amplitude? Answer: difference
@@ -29,27 +29,27 @@ const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 // - Fix min value in temp array
 // - Subtract min form max (amplitude) and return it
 
-const calcTempAmplitudeNew = function (t1, t2) {
-  const temps = t1.concat(t2);
-  console.log(temps);
+// const calcTempAmplitudeNew = function (t1, t2) {
+//   const temps = t1.concat(t2);
+//   console.log(temps);
 
-  let max = temps[0];
-  let min = temps[0];
+//   let max = temps[0];
+//   let min = temps[0];
 
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== "number") continue;
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== "number") continue;
 
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
-  }
-  console.log(max, min);
-  return max - min;
-};
-// calcTempAmplitude([3, 7, 4, 1, 8]);
-// calcTempAmplitude(temperatures);
-const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
-console.log(amplitudeNew);
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+// // calcTempAmplitude([3, 7, 4, 1, 8]);
+// // calcTempAmplitude(temperatures);
+// const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+// console.log(amplitudeNew);
 
 // output
 // [ 3, 5, 1, 9, 0, 5 ]
@@ -77,3 +77,5 @@ console.log(amplitudeNew);
 // animals.push("chickens", "cats", "dogs");
 // console.log(animals);
 // // output  ["pigs", "goats", "sheep", "cows", "chickens", "cats", "dogs"];
+
+// 60. Debugging(Fixing Errors)
