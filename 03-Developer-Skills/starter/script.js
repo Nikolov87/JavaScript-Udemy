082474939db6e20771c523050f08a1b8e719deb2
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-05-31 15:39:31 +0100

    62.Coding Challenge #1

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -81,47 +81,94 @@
 // 60. Debugging(Fixing Errors)
 
 // 62. Debugging with the Console and Breakpoints
-const measureKelvin = function () {
-  const measurement = {
-    type: "temp",
-    unit: "celsium",
-    // C) FIX the BUG
-    // value: Number(prompt("Degrees celsius:")),
-    value: 10,
-  };
+// const measureKelvin = function () {
+//   const measurement = {
+//     type: "temp",
+//     unit: "celsium",
+//     // C) FIX the BUG
+//     // value: Number(prompt("Degrees celsius:")),
+//     value: 10,
+//   };
 
-  //   console.log(measurement);
-  // B) FIND the BUG
-  console.table(measurement);
+//   //   console.log(measurement);
+//   // B) FIND the BUG
+//   console.table(measurement);
 
-  //   console.log(measurement.value);
-  const kelvin = measurement.value + 273;
-  return kelvin;
-};
-// A) Idnetify the BUG
-console.log(measureKelvin());
+//   //   console.log(measurement.value);
+//   const kelvin = measurement.value + 273;
+//   return kelvin;
+// };
+// // A) Idnetify the BUG
+// console.log(measureKelvin());
 
-//Using a debugger
-const calcTempAmplitudeBug = function (t1, t2) {
-  const temps = t1.concat(t2);
-  console.log(temps);
+// //Using a debugger
+// const calcTempAmplitudeBug = function (t1, t2) {
+//   const temps = t1.concat(t2);
+//   console.log(temps);
 
-  let max = 0;
-  let min = 0;
+//   let max = 0;
+//   let min = 0;
 
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== "number") continue;
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== "number") continue;
 
-    // debugger;
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
-  }
-  console.log(max, min);
-  return max - min;
-};
-// calcTempAmplitude([3, 7, 4, 1, 8]);
-// calcTempAmplitude(temperatures);
-const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
-// A) IDENTIFY the BUG
-console.log(amplitudeBug);
+//     // debugger;
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+// // calcTempAmplitude([3, 7, 4, 1, 8]);
+// // calcTempAmplitude(temperatures);
+// const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// // A) IDENTIFY the BUG
+// console.log(amplitudeBug);
+
+///////////////////////////////////////
+// 62.Coding Challenge #1
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+// function printForecast(arr) {
+//   let forecastString = "";
+
+//   for (let i = 0; i < arr.length; i++) {
+//     forecastString += `... ${arr[i]}ºC in ${i + 1} days`;
+//   }
+
+//   console.log(forecastString);
+// }
+
+// const testData1 = [17, 21, 23];
+// const testData2 = [12, 5, -5, 0, 4];
+
+// 2) Breaking up into sub-problem
+// - Transform array into string
+// - Transform each element to string with ºC
+// - String needs to contain day (index + 1)
+// - Add ... between element and start and end of string
+// - Log string to console
+
+// const data1 = [17, 21, 23];
+// const data2 = [12, 5, -5, 0, 4];
+
+// console.log(`... ${data1[0]}ºC ... ${data1[1]}ºC ... ${data1[2]}ºC ...`);
+// const printForecast = function (arr) {
+//   let str = "";
+//   for (let i = 0; i < arr.length; i++) {
+//     str = str + `${arr[i]}ºC in ${i + 1} days`;
+//   }
+//   console.log("..." + str);
+// };
+// printForecast(data1);
