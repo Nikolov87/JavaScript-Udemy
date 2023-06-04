31dcdaff35b929bc705688f319831e82377fae5d
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-06-04 14:56:11 +0100

    69.Implementing Highscores

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -14,6 +14,7 @@ let score = 20;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
+  let highscore = 0;
 
   // If there is no input
   if (!guess) {
@@ -27,6 +28,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // When guess is too high
   } else if (guess > secretNumber) {
