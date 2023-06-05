061a6f8d37afe83407556f4c001abbe06782a1be
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-06-05 18:30:47 +0100

    74. PROJECT #3:Pig Game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+//Selecting elements
+const score0EL = document.querySelector('#score--0');
+const score1EL = document.getElementById('score--1');
+const diceEL = document.querySelector('.dice');
+
+//Starting condition
+score0EL.textContent = 0;
+score1EL.textContent = 0;
+diceEL.classList.add('hidden');
+// document.getElementsByClassName
+// ('className')
+// [0].style.visibility='hidden';
