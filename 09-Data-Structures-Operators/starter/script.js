88988325a7bcc87132d6ec03c164a19d391fd67d
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-07-01 00:23:34 +0100

    128.Default Parameters

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -54,7 +54,7 @@ const restaurant = {
 
 ///////////////////////////////////////
 // 125.String Methods Practice
-
+/*
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
@@ -73,6 +73,7 @@ for (const flight of flights.split('+')) {
   )} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`.padStart(36);
   console.log(output);
 }
+*/
 
 ///////////////////////////////////////
 // Coding Challenge #4
--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// 128.Default Parameters
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengeres = 1,
+  price = 200 * numPassengeres
+) {
+  // ES5
+  //   numPassengeres = numPassengeres || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengeres,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123'); // {flightNum: 'LH123', numPassengeres: 1, price: 200}
+createBooking('LH123', 2, 700); // {flightNum: 'LH123', numPassengeres: 2, price: 700}
+createBooking('LH123', 3); // {flightNum: 'LH123', numPassengeres: 3, price: 600}
+createBooking('LH123', 5); // {flightNum: 'LH123', numPassengeres: 5, price: 1000}
