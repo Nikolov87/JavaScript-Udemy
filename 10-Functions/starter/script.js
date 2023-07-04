b5bfacc33b247513e82d35bab8adc51b04d02e8f
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-07-04 11:25:43 +0100

    134. The bind 🤯 method

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,7 +1,11 @@
 'use strict';
 
 ////////////////////////////
+// 134. The bind 🤯 method
+
+//////////////////////////////////
 // 133. The call and apply Methods
+
 const lufthansa = {
   airline: 'Lufthansa',
   iataCode: 'LH',
@@ -55,8 +59,54 @@ console.log(swiss);
 
 book.call(swiss, ...flightData); // Tintiri Mintiri booked a seat on Swiss Air Lines flight LX453
 
+// 134. The bind 🤯 method
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Stiven Williams'); // Stiven Williams booked a seat on Eurowings flight EW23
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Petar Nikolov');
+bookEW23('Marta Cooper');
+
+// With Event Listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application - partial means to preset parameters
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100)); // 123
+console.log(addVAT(23)); // 28.29
+
+// the same but in variant function returning function
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100)); // 123
+console.log(addVAT2(23)); // 28.29
+
 //////////////////////////////
-// 132. Functions Returning Functions
+// 132. Function Returning Function
 /*
 const greet = function (greeting) {
   return function (name) {
