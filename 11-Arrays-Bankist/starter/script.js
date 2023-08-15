1c53458f2ab383408b8ada22ecc4a38d61116f2e
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-08-15 15:31:57 +0100

    166.Array Methods Practice

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -702,30 +702,30 @@ console.log(movements); // (8) [3000, 1300, 450, 200, 70, -130, -400, -650]
 // 🔴 164.More ways of Creating and Filling Arrays
 
 const arr = [1, 2, 3, 4, 5, 6, 7]; // (7) [1, 2, 3, 4, 5, 6, 7]
-console.log(new Array(1, 2, 3, 4, 5, 6, 7)); // (7) [1, 2, 3, 4, 5, 6, 7]
+// console.log(new Array(1, 2, 3, 4, 5, 6, 7)); // (7) [1, 2, 3, 4, 5, 6, 7]
 
 // Empty arrays + fill method
 
 const x = new Array(7); // 7 empty arrays
-console.log(x); // (7) [empty × 7]
+// console.log(x); // (7) [empty × 7]
 // console.log(x.map(() => 5)); // the same (7) [empty × 7] so we can't use map
 
 // x.fill(1); // (7) [1, 1, 1, 1, 1, 1, 1]
 x.fill(1, 3, 5); // (7) [empty × 3, 1, 1, empty × 2]
 x.fill(1); // (7) [1, 1, 1, 1, 1, 1, 1]
-console.log(x);
+// console.log(x);
 
 arr.fill(23, 4, 6); // (7) [1, 2, 3, 4, 23, 23, 7]
-console.log(arr);
+// console.log(arr);
 
 // Array.from
 const y = Array.from({ length: 7 }, () => 1);
 // here we have length: 7 and this means 7 arrays passing to callback function
 // in every array in side will be 1 // (7) [1, 1, 1, 1, 1, 1, 1]
-console.log(y);
+// console.log(y);
 
 const z = Array.from({ length: 7 }, (_, i) => i + 1);
-console.log(z); // (7) [1, 2, 3, 4, 5, 6, 7]
+// console.log(z); // (7) [1, 2, 3, 4, 5, 6, 7]
 
 // getting the sum from the interface because we dont have them in
 // our code and we will and doing the calculation based on that
@@ -840,3 +840,65 @@ labelBalance.addEventListener('click', function () {
 // - Based on callback
 // .forEach
 // (Does not create a new array, just loop over it)
+
+// 🔴 166.Array Methods Practice
+
+// 1.
+const bankDepositSum = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov > 0)
+  .reduce((sum, cur) => sum + cur, 0);
+
+console.log(bankDepositSum);
+
+// 2.
+// const numDeposits1000 = accounts
+//   .flatMap(acc => acc.movements)
+//   .filter(mov => mov >= 1000).length;
+
+const numDeposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  // .reduce((count, cur) => (cur >= 1000 ? count + 1 : count), 0);
+  .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
+
+console.log(numDeposits1000);
+
+// // Prefixed ++ operator
+// let a = 10;
+// console.log(a++); // 10
+// console.log(++a); // 11
+// console.log(a); // 11
+
+// 3.
+const { deposits, withdrowals } = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, cur) => {
+      // cur > 0 ? (sums.deposits += cur) : (sums.withdrowals += cur);
+      sums[cur > 0 ? 'deposits' : 'withdrowals'] += cur;
+      return sums;
+    },
+    { deposits: 0, withdrowals: 0 }
+  );
+
+console.log(deposits, withdrowals);
+
+// 4.
+// this is a nice title -> This Is a Nice Title
+const convertTitleCase = function (title) {
+  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+
+  const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => (exceptions.includes(word) ? word : capitalize(word)))
+    .join(' ');
+
+  return capitalize(titleCase);
+};
+
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
