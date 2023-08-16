aed70d7b9c05d0d73199cd5f7ab3172c95e66bf1
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-08-16 12:11:56 +0100

    173. Numeric Separator

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -343,6 +343,7 @@ console.log(+(2.345).toFixed(2)); // converting the string to number adding +
 
 // 🔴 172. The Remainder Operator
 // оператор за остатък
+/*
 console.log(5 % 2); // output 1
 console.log(5 / 2); // 2.5 // 5 = 2 * 2 + 1
 
@@ -368,3 +369,24 @@ labelBalance.addEventListener('click', function () {
     if (i % 3 === 0) row.style.backgroundColor = 'blue';
   });
 });
+
+*/
+
+// 🔴 173. Numeric Separator
+/*
+// 287,460,000,000
+const diameter = 287_460_000_000;
+console.log(diameter); // 287460000000
+
+const price = 345_99;
+console.log(price); // 34599
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.1415;
+console.log(PI); // 3.1415
+
+console.log(Number('230_000')); // NaN
+console.log(parseInt('230_000')); // 230
+*/
