5d60aaa81adc861eb7d770bc6af8b6512a0d8d10
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-08-18 12:00:16 +0100

    🔴 186. Selecting, Creating and Deleting Elemets

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -37,3 +37,44 @@ document.addEventListener('keydown', function (e) {
 // and deleting for loop
 
 // 🔴 185. How the DOM Really Works
+
+// 🔴 186. Selecting, Creating and Deleting Elemets
+
+// Selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+// Create and inserting elements
+// .insertAdjacentHTML - for more info see file ReadMe.md
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality and analytics.';
+
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+// Ddelete elements
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    // message.remove(); // after clicking of the message, the message is gone
+    message.parentElement.removeChild(message);
+  });
