3bde43569725e2cad22346910daaa3b1d5d6b6fd
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-08-20 15:44:33 +0100

    🔴 195. Passing Arguments to Event Handlers

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -8,6 +8,17 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
+// from
+// 🔴 195. Passing Arguments to Event Handlers
+// Menu fade animation
+const nav = document.querySelector('.nav');
+
+// 🔴 194. Building a Tabbed Component
+// Clicking on the different tab(button) the content is changed
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
 ///////////////////////////////////////
 // Modal window
 
@@ -319,9 +330,8 @@ console.log(h1.parentElement.children); // in this way we have ALL the elements(
 // 🔴 194. Building a Tabbed Component
 // Clicking on the different tab(button) the content is changed
 
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
+// go to line 16
+//////////////////////////////////////
 
 // Taking all the TABs and attack event handler
 // tabs.forEach(t => t.addEventListener('click', () => console.log('TAB')));
@@ -344,3 +354,23 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+// 🔴 195. Passing Arguments to Event Handlers
+// GO TO LINE 12
+// Menu fade animation
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+// Passing 'argument' into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
