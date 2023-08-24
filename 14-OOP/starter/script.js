f3f44a5cce2af8b04cdf104e5994a318658430b6
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-08-24 17:03:57 +0100

    🚨 216. Object.create

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -212,6 +212,7 @@ console.log(account.movements); // (5) [200, 300, 500, 340, 50]
 */
 
 // 🚨 215. Static Methods
+/*
 const Person = function (firstName, birthYear) {
   // instance properties
   this.firstName = firstName;
@@ -224,3 +225,28 @@ Person.hey = function () {
 };
 
 Person.hey();
+*/
+
+// 🚨 216. Object.create
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto); // true
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge(); // 58
