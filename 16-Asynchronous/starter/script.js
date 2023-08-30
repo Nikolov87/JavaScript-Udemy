b36b4db4f7994b7be08b2ab12ef49bd7cd6f0357
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-08-30 21:37:31 +0100

    👻 250.Promises and the Fetch API

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -10,6 +10,7 @@ const countriesContainer = document.querySelector('.countries');
 // 👻 248. Our First AJAX Call: XMLHttpRequest 🇧🇬
 
 // the old way calling AJAX in JS
+/*
 const getCountryData = function (country) {
   const request = new XMLHttpRequest();
   request.open(
@@ -44,8 +45,79 @@ const getCountryData = function (country) {
 };
 
 getCountryData('bulgaria');
-getCountryData('portugal');
-getCountryData('poland');
-getCountryData('germany');
-
+*/
 // 👻 249.[OPTIONAL] How the Web Works: Requests and Responses
+// 👻 250.Promises and the Fetch API
+
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+    </article>
+    `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open(
+    'GET',
+    `https://countries-api-836d.onrender.com/countries/name/${country}`
+  );
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country (2)
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open(
+      'GET',
+      `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
+    );
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+// getCountryAndNeighbour('bulgaria');
+getCountryAndNeighbour('russia');
+
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 second passed');
+    setTimeout(() => {
+      console.log('3 second passed');
+      setTimeout(() => {
+        console.log('4 second passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
