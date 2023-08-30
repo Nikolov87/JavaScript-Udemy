6669ace1feb037f4851461a8edc738a1102526f4
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-08-30 13:40:43 +0100

    👻 246. Asynchronous JavaScript, AJAX and APIs

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,4 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+// 👻 246. Asynchronous JavaScript, AJAX and APIs
