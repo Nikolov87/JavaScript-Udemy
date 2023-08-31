cb23c4bbcd9693ddb50a9f5a34f18d579fd077ac
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-08-31 14:41:37 +0100

    👻 254. Handling Rejected Promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,6 +3,30 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function (data, className = '') {
+  const html = `
+      <article class="country ${className}">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+          ).toFixed(1)} people</p>
+          <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+          <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+        </div>
+      </article>
+      `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  //   countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  //   countriesContainer.style.opacity = 1;
+};
+
 ///////////////////////////////////////
 
 // 👻 246. Asynchronous JavaScript, AJAX and APIs
@@ -48,25 +72,6 @@ getCountryData('bulgaria');
 */
 // 👻 249.[OPTIONAL] How the Web Works: Requests and Responses
 // 👻 250. Welcome to Callback Hell
-
-const renderCountry = function (data, className = '') {
-  const html = `
-    <article class="country ${className}">
-      <img class="country__img" src="${data.flag}" />
-      <div class="country__data">
-        <h3 class="country__name">${data.name}</h3>
-        <h4 class="country__region">${data.region}</h4>
-        <p class="country__row"><span>👫</span>${(
-          +data.population / 1000000
-        ).toFixed(1)} people</p>
-        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-      </div>
-    </article>
-    `;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
 
 /*
 const getCountryAndNeighbour = function (country) {
@@ -155,7 +160,6 @@ setTimeout(() => {
 // };
 
 // the 3️⃣ way - the most simple
-
 const getCountryData = function (country) {
   // Country 1
   fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`) // there we fetch something
@@ -174,6 +178,18 @@ const getCountryData = function (country) {
       );
     })
     .then(response => response.json())
-    .then(data => renderCountry(data, 'neighbour'));
+    .then(data => renderCountry(data, 'neighbour'))
+    .catch(err => {
+      console.error(`${err} 💥`);
+      renderError(`Something went wrong 💥 💥 💥 ${err.message}. Try again!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
 };
-getCountryData('germany');
+// 👻 254. Handling Rejected Promises // .catch(err => alert(err)); end of the chain
+btn.addEventListener('click', function () {
+  getCountryData('bulgaria');
+});
+
+getCountryData('dsfsdfd');
