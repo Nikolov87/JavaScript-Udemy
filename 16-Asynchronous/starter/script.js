859100a35cb2d887e6d9466a5af6c3a7fa58dcea
Petar Nikolov <100172752+Nikolov87@users.noreply.github.com>
2023-08-31 23:03:36 +0100

    👻 258. The Event Loop in Practice

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -338,7 +338,7 @@ GOOD LUCK 😀
 // whereAmI(-33.933, 18.474); // Example 3
 
 // Jona's SOLUTION
-
+/*
 const whereAmI = function (lat, lng) {
   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
     .then(res => {
@@ -362,3 +362,23 @@ const whereAmI = function (lat, lng) {
 whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
+*/
+
+// 👻 257.Asynchronous Behind the Scenes: The Event Loop
+// 👻 258. The Event Loop in Practice
+
+console.log('Test start');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 10000; i++) console.log(res);
+});
+
+console.log('Test end');
+
+// OUTPUT 👇
+// Test start
+// Test end
+// Resolved promise 1
+// 0 sec timer
